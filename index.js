79e66b1ccb6a3a3ec213521db5d5eda0b905e58e
John <johnmulvey48@yahoo.com>
2021-11-12 08:18:45 -0500

    git@github.com:Jmulvez/phase-0-javascript-events-acting-on-events-lab.git

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(leftNumbers, 10);
+  
+    if (right > 0) {
+      dodger.style.right = `${left - 1}px`;
+    }
+  }
+  undefined
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  undefined
